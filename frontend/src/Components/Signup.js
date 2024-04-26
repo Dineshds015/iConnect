c5af7bdb6922569b5ae034f9030503881b6f4e99
NitumoniMech <123485810+NitumoniMech@users.noreply.github.com>
2024-04-26 07:52:46 +0000

    Login and signup fixed

--- frontend/src/Components/Signup.js
+++ frontend/src/Components/Signup.js
@@ -55,7 +55,7 @@ const Signup = () => {
   const handleVerifyClick = async () => {
     const formErrors = validateFormData(formData); // Validate the form data
     const response=await sendEmailToOtp();
-    console.log("res" ,response.data);
+    // console.log("res" ,response.data);
     if (Object.keys(formErrors).length === 0) {
       
       if (response && response.data.message==="existingUser"){
@@ -141,41 +141,44 @@ const Signup = () => {
       transition="Bounce"
       />
     <ToastContainer/>
-    <div className="flex flex-row justify-center shadow-lg">
-      <div className="flex flex-col">
-        <img className="rounded-2xl m-4" src={logo} alt="logo" />
-        <form className='flex flex-col'>
+    <div className="w-[90%] ml-8 sm:ml-0 h-80vh flex flex-col sm:w-96 md:w-1/2 xl:w-1/3 items-center absolute top-72  sm:left-1/4 md:left-1/4 xl:left-[35%]   bg-white  shadow-2xl rounded-3xl">
+      
+        <img className="rounded-2xl h-52 w-52 mx-4" src={logo} alt="logo" />
+        <form className='flex flex-col w-[100%]'>
           <input className='mx-4 px-4 my-2 border border-solid w-auto h-12 rounded-full' type="text" name="name" placeholder='Enter Your Name' onChange={handleInputChange} disabled={showOtp}/>
-          {errors.name && <span className="text-red-500">{errors.name}</span>}
+          {errors.name && <span className="ml-auto mr-6 text-red-600">{errors.name}</span>}
           <input className='mx-4 px-4 my-2 border border-solid w-auto h-12 rounded-full' type="text" name="email" placeholder='Enter Your Email' onChange={handleInputChange} disabled={showOtp}/>
-          {errors.email && <span className="text-red-500">{errors.email}</span>}
+          {errors.email && <span className="ml-auto mr-6 text-red-600">{errors.email}</span>}
           <input className='mx-4 px-4 my-2 border border-solid w-auto h-12 rounded-full' type="password" name="password" placeholder='Enter Your Password' onChange={handleInputChange} disabled={showOtp}/>
-          {errors.password && <span className="text-red-500">{errors.password}</span>}
+          {errors.password && <span className="ml-auto mr-6 text-red-600">{errors.password}</span>}
           <input className='mx-4 px-4 my-2 border border-solid w-auto h-12 rounded-full' type="password" name="cPassword" placeholder='Confirm Password' onChange={handleInputChange} disabled={showOtp}/>
-          {errors.cPassword && <span className="text-red-500">{errors.cPassword}</span>}
+          {errors.cPassword && <span className="ml-auto mr-6 text-red-600">{errors.cPassword}</span>}
 
           {!showOtp ? (
             <button className='mx-4 px-4 my-2 mb-8 border border-solid w-auto h-12 rounded-full bg-blue-500 font-bold text-2xl' type="button" onClick={handleVerifyClick}>Verify</button>
           ) : (
-            <div className='w-full'>
-              <div className='flex justify-center'>
-                <div className='w-1/2'>
+            <div className='flex flex-col'>
+              <div className='flex flex-row'>
+
                   <input className='mx-4 px-4 my-2 border border-solid w-auto h-12 rounded-full' type="password" name="otp" placeholder='Enter 6 digit OTP' onChange={handleInputChange} />
-                  <button type="button" onClick={resendOTP}>Resend OTP</button>
-                </div>
-                <div className='w-1/2'>
-                  <button className='mx-4 px-4 my-2 pb-0 border border-solid w-auto h-12 rounded-full bg-blue-500 font-bold text-2xl' type="submit" onClick={submitReg}>Register</button>
-                </div>
+                  <button className='-ml-20 px-4 my-2 pb-0 border border-solid w-auto h-12 rounded-full bg-blue-500 font-bold text-2xl' type="submit" onClick={submitReg}>Register</button>
+                  </div>
+
+                <button className='mx-4 cursor-pointer mb-8' type="button" onClick={resendOTP}>Resend OTP</button>
+
+                  {/* <button className='mx-4 px-4 my-2 pb-0 border border-solid w-auto h-12 rounded-full bg-blue-500 font-bold text-2xl' type="submit" onClick={submitReg}>Register</button> */}
+
               </div>
-            </div>
+
           )}
         </form>
-      </div>
-      <div className='flex flex-col justify-center m-10 mt-44'>
+
+      {/* <div className='flex flex-col justify-center m-10 mt-44'>
         <button className='mx-4 px-4 my-2 border border-solid w-96 h-12 rounded-full bg-teal-300 text-2xl flex items-center justify-center' type="submit"><img className='h-8  mr-2' src={google} alt='Google Logo' />Login In with Google</button>
         <button className='mx-4 px-4 my-2 border border-solid w-96 h-12 rounded-full bg-teal-300 text-2xl flex items-center justify-center' type="submit"><img className='h-8 border border-solid mr-2' src='https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg' alt='LinkedIn Logo' />Login In with LinkedIn</button>
         <Link to="/Login" className='ml-32 cursor-pointer'>Already have an account? Sign In</Link>
-      </div>
+      </div> */}
+      <Link to="/Login" className=' cursor-pointer mb-4 text-blue-400 font-semibold'>Already have an account? Sign In</Link>
     </div>
     </>
   )
